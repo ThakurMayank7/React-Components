d5db49f32f95e75018f6014496ea2bef699722c2
Mayank <mayanksingh8181335335@gmail.com>
2024-11-18 13:23:32 +0530

    added Grid and GridBlock

---
+++ Grid.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import GridBlock from './GridBlock';
+
+type GridProps = {
+    darkmode: boolean;
+    grid: string[];
+    clickBlock: (block: number) => void;
+  };
+
+function Grid({ darkmode, grid, clickBlock }: GridProps) {
+
+    const clickedBlock = (gridId: number) => {
+        clickBlock(gridId);
+      };
+
+  return (
+    <div
+      id="Banner"
+      className={`flex justify-center items-center p-2 ${
+        darkmode ? "bg-gray-950" : ""
+      }`}
+    >
+      <div id="board" className="grid grid-cols-3 aspect-square h-96 gap-2">
+        <GridBlock
+          gridId={1}
+          darkM={darkmode}
+          text={grid[0]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={2}
+          darkM={darkmode}
+          text={grid[1]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={3}
+          darkM={darkmode}
+          text={grid[2]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={4}
+          darkM={darkmode}
+          text={grid[3]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={5}
+          darkM={darkmode}
+          text={grid[4]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={6}
+          darkM={darkmode}
+          text={grid[5]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={7}
+          darkM={darkmode}
+          text={grid[6]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={8}
+          darkM={darkmode}
+          text={grid[7]}
+          click={clickedBlock}
+        />
+        <GridBlock
+          gridId={9}
+          darkM={darkmode}
+          text={grid[8]}
+          click={clickedBlock}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Grid
---
+++ GridBlock.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+type GridBlockProps = {
+    darkM: boolean;
+    gridId: number;
+    click: (id: number) => void;
+    text: string;
+  };
+
+function GridBlock({ darkM, gridId, click, text }: GridBlockProps) {
+
+    const func = () => {
+        click(gridId);
+      };
+
+  return (
+    <div
+      className={`${
+        darkM ? "bg-gray-600" : "bg-teal-300"
+      } rounded aspect-square flex items-center justify-center max-h-32 
+        hover:${darkM ? "bg-gray-900" : "bg-teal-700"} text-8xl cursor-pointer`}
+      onClick={func}
+    >
+      {text}
+    </div>
+  )
+}
+
+export default GridBlock
